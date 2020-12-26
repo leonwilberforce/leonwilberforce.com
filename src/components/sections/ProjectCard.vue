113e089f2690b8cbcf78975f2a3f67bfa8165d8f
Leon Wilberforce <leonwilberforce@outlook.com>
2020-12-26 21:58:27 +0000

    Created a project card element

---
+++ src/components/sections/ProjectCard.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="project card">
+    <div class="row">
+      <div class="col-md-6">
+        <div
+          class="project-image text-left"
+          :style="{ backgroundImage: 'url(' + project.imageUrl + ')' }"
+        >
+          <i
+            v-for="technology in project.technologies"
+            v-bind:key="technology"
+            :class="technology.iconClass"
+          ></i>
+        </div>
+      </div>
+      <div class="col-md-6 d-flex flex-column">
+        <div class="project-detail">
+          <h3>{{ project.title }}</h3>
+          <p class="mb-xl-0">
+            {{ project.description }}
+          </p>
+        </div>
+        <a
+          v-if="project.link"
+          :href="project.link"
+          class="btn btn-primary mt-auto project-link"
+          >View Project</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: {
+    project: Object
+  },
+  computed: {}
+};
+</script>
+
+<style scoped>
+.project.card {
+  overflow: hidden;
+  color: black;
+  margin: 0 10px;
+}
+
+.project.card .project-detail {
+  padding: 20px 20px 0 20px;
+}
+
+.project.card .project-detail h3 {
+  font-size: 20px;
+}
+
+.project.card .project-detail p {
+  font-size: 14px;
+}
+
+.project.card .project-image {
+  height: 200px;
+  width: 100%;
+  /* background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 0) 70%,
+    rgb(255, 255, 255) 100%
+  ); */
+  background-size: cover;
+  background-position: center;
+}
+
+.project-image i {
+  font-size: 25px;
+  text-align: left;
+  color: #eaf1ffd0;
+  margin: 8px;
+}
+
+.project-link {
+  margin-bottom: 15px;
+  margin-right: 30px;
+}
+
+/* Small devices (landscape phones, less than 768px) */
+@media (max-width: 767.98px) {
+  .project-link {
+    margin-left: 30px;
+  }
+}
+</style>
--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -2,83 +2,82 @@
   <section id="projects" class="small-section container-fluid">
     <a class="anchor" id="projects-nav"></a>
     <h1>Projects</h1>
-    <div class="row justify-content-md-center text-center">
-      <div class="col-6 mt-3">
-        <div class="project card">
-          <div class="row">
-            <div class="col-6">
-              <div class="project-image text-left">
-                <i class="fab fa-java"></i>
-                <i class="fab fa-node-js"></i>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="project-detail">
-                <h3>Discord Studybot</h3>
-                <p>
-                  When completing my studies at college, we were given
-                  assignments using python. I went further to extend my
-                  knowledge by using the discord.py API and SQLite to create a
-                  discord assignment tracker.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mt-3">
-        <div class="project card">
-          <div class="row">
-            <div class="col-6">
-              <div class="project-image text-left">
-                <i class="fab fa-java"></i>
-                <i class="fab fa-node-js"></i>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="project-detail">
-                <h3>Landscaping</h3>
-                <p>
-                  I developed this website as part of a course in 2017, This was
-                  a good learning tool as the unique CSS was used to produce
-                  different elements.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 mt-3">
-        <div class="project card">
-          <div class="row">
-            <div class="col-6">
-              <div class="project-image text-left">
-                <i class="fab fa-java"></i>
-                <i class="fab fa-node-js"></i>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="project-detail">
-                <h3>Sorting Algorithms</h3>
-                <p>
-                  During my first year of university, I was tasked with making a
-                  visualiser in Java to show how the different sorting
-                  algorithms work. I did this using an array of JLabels to
-                  represent the bar values. You can find this project on my
-                  GitHub.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="row justify-content-center text-center">
+      <div class="col-xl-6 mt-5" v-for="project in projects" v-bind:key="project">
+        <ProjectCard :project="project" />
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import ProjectCard from "@/components/sections/ProjectCard.vue";
+
 export default {
-  name: "Projects"
+  name: "Projects",
+  components: {
+    ProjectCard
+  },
+  data: function() {
+    return {
+      projects: [
+        {
+          title: "Discord Studybot",
+          description: `A discord bot that is used to track assignments and deadlines in college. Built using Python and the discord.py API.`,
+          imageUrl: require("../../assets/images/projects/3.png"),
+          link: null,
+          technologies: [
+            {
+              name: "Python",
+              color: "#fff",
+              iconClass: "fab fa-python"
+            },
+            {
+              name: "Discord Bot",
+              color: "#fff",
+              iconClass: "fab fa-discord"
+            }
+          ]
+        },
+        {
+          title: "Landscaping",
+          description: `A PHP website designed as part of a course in 2017. Built using HTML, CSS and PHP`,
+          imageUrl: require("../../assets/images/projects/1.png"),
+          link: "https://landscaping.sites.leonwilberforce.com/",
+          technologies: [
+            {
+              name: "PHP",
+              color: "#fff",
+              iconClass: "fab fa-php"
+            },
+            {
+              name: "CSS",
+              color: "#fff",
+              iconClass: "fab fa-css3-alt"
+            },
+            {
+              name: "HTML",
+              color: "#fff",
+              iconClass: "fab fa-html5"
+            }
+          ]
+        },
+        {
+          title: "Sorting Algorithms",
+          description: `A sorting algorithm GUI visualiser. Built using Java and the Swing library. Supported algorithms: bubble, shuttle and shaker.`,
+          imageUrl: require("../../assets/images/projects/2.png"),
+          link: "https://github.com/leonwilberforce/GUI-Sorting-Algorithms",
+          technologies: [
+            {
+              name: "Java",
+              color: "#fff",
+              iconClass: "fab fa-java"
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
 
@@ -89,47 +88,8 @@ export default {
   color: #eaf1ff;
 }
 
-.project.card {
-  height: 200px;
-  overflow: hidden;
-  color: black;
-  margin: 0 10px;
-}
-
 .container-fluid {
   padding-left: 10%;
   padding-right: 10%;
-}
-
-.project.card .project-detail {
-  padding: 20px;
-}
-
-.project.card .project-detail h3 {
-  font-size: 20px;
-}
-
-.project.card .project-detail p {
-  font-size: 14px;
-}
-
-.project.card .project-image {
-  height: 200px;
-  width: 100%;
-  background: linear-gradient(
-      90deg,
-      rgba(2, 0, 36, 0) 70%,
-      rgb(255, 255, 255) 100%
-    ),
-    url(https://leonwilberforce.com/Resources/images/sorting.png);
-  background-size: cover;
-  background-position: center;
-}
-
-.project-image i {
-  font-size: 25px;
-  text-align: left;
-  color: #eaf1ffd0;
-  margin: 8px;
 }
 </style>
