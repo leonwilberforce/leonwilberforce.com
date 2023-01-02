c72d63115182b7481e074d707db0c243e2668cd2
Leon Wilberforce <leonwilberforce@outlook.com>
2023-01-02 22:22:41 +0000

    Implement plausible

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { createHead } from "@vueuse/head";
+import {Vue3Plausible} from "vue-plausible";
 
 const head = createHead();
 
@@ -14,4 +15,10 @@ createApp(App)
   .use(store)
   .use(router)
   .use(head)
+  .use(Vue3Plausible, {
+    domain: process.env.VUE_APP_DOMAIN,
+    apiHost: process.env.VUE_APP_PLAUSIBLE_API,
+    enableAutoPageviews: true,
+    enableAutoOutboundTracking: true
+  })
   .mount("#app");
